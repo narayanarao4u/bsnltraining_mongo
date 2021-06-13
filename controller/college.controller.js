e2dc581b4704854458618f664fb1c4dcd83e18d7
Narayana Rao <narayanarao4u@yahoo.com>
2021-06-13 14:28:49 +0530

    College List

---
+++ controller/college.controller.js
@@ -0,0 +1,51 @@
+const Document = require('../data.model').College;
+
+
+exports.index =  function(req, res){
+
+    Document.find((err, result) => {
+        if (!err)
+            res.json({ msg: 'Data Retrive Success', data: result });
+        else
+            res.json({ msg: 'Data Retrive failed', err : err });
+    });
+
+}
+
+exports.DataSort =  function(req, res){
+  Document.find({}).sort({'collegeName':1}).exec(function(err, result) {
+      if (!err)
+          res.json({ msg: 'Data Retrive Success', data: result });
+      else
+          res.json({ msg: 'Data Retrive failed', err : err });
+  });
+
+}
+
+exports.new =  function(req, res){
+    var doc = new Document(req.body);
+    doc.save((err, result) => {
+        if (!err)
+            res.json({ msg: 'Insert Success...', data: result });
+        else
+            res.json({ msg: 'Error during record insertion ', err : err });
+    });
+
+}
+
+exports.update =  function(req, res){
+    Document.findOneAndUpdate({ _id: req.body._id }, req.body, { new: true }, (err, result) => {
+        if (!err) { res.json({msg :'Update Success', data : result}); }
+        else { res.json({msg:'failed to update : ', err: err}); }
+    });
+
+}
+
+
+exports.delete =  function(req, res){
+    Document.findByIdAndRemove(req.body._id, (err, result) => {
+        if (!err) { res.json({msg :'Delete Success'}); }
+        else { res.json({msg:'failed to Delete : ', err: err}); }
+    });
+
+}
